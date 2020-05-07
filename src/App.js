14939eeb5550b92b899081ad35e290ce96e8077e
yash <yashp6765@gmail.com>
2020-05-07 13:31:57 +0530

    other files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React , {component} from 'react';
+
+import './App.css';
+import Demo from './Demo';
+
+import Home from './Home';
+import Project from './project'
+import {BrowserRouter ,Route , Switch} from 'react-router-dom';
+function App() {
+  
+  return (<BrowserRouter>
+  <div>
+        <Demo/>
+        
+        <Switch>
+          
+          <Route exact path='/' exact component={Home}></Route>
+          <Route exact path='/project' component={Project}></Route>
+      
+        </Switch>
+</div>
+    </BrowserRouter>
+    
+
+  );
+
+}
+
+export default App;
