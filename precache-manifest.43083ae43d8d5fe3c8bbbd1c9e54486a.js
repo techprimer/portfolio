6bed2b2817d1128beb09eb937e3ed58de0b4b03f
yash <yashp6765@gmail.com>
2020-05-07 17:55:17 +0530

    Updates

---
+++ precache-manifest.43083ae43d8d5fe3c8bbbd1c9e54486a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df62b76e2522ae24c8db3855c0c4a64",
+    "url": "/techprimer.github.io/index.html"
+  },
+  {
+    "revision": "ab10c24a5bc799af383e",
+    "url": "/techprimer.github.io/static/css/2.5942f7ea.chunk.css"
+  },
+  {
+    "revision": "36234fc2b7f2a1546ae3",
+    "url": "/techprimer.github.io/static/css/main.fa765bb6.chunk.css"
+  },
+  {
+    "revision": "ab10c24a5bc799af383e",
+    "url": "/techprimer.github.io/static/js/2.f6a5865e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/techprimer.github.io/static/js/2.f6a5865e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36234fc2b7f2a1546ae3",
+    "url": "/techprimer.github.io/static/js/main.da83a979.chunk.js"
+  },
+  {
+    "revision": "9ef6908eca225d1f6bcb",
+    "url": "/techprimer.github.io/static/js/runtime-main.4f603215.js"
+  },
+  {
+    "revision": "d2d7bac0980953cc2f981b1c95cabc74",
+    "url": "/techprimer.github.io/static/media/Resume.d2d7bac0.pdf"
+  },
+  {
+    "revision": "05902294f9c483b5f234454255a1465b",
+    "url": "/techprimer.github.io/static/media/designicon.05902294.png"
+  },
+  {
+    "revision": "1fb3be47b24db16901187190a7bc8708",
+    "url": "/techprimer.github.io/static/media/development.1fb3be47.png"
+  },
+  {
+    "revision": "8e114dc6d18c985a3cba54c403596688",
+    "url": "/techprimer.github.io/static/media/divimage.8e114dc6.jpg"
+  },
+  {
+    "revision": "de7c127720f2ac94f4c55df7991af06b",
+    "url": "/techprimer.github.io/static/media/ecom.de7c1277.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/techprimer.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/techprimer.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/techprimer.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/techprimer.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/techprimer.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af2d40974973ba0928caea13e0addea6",
+    "url": "/techprimer.github.io/static/media/hms.af2d4097.png"
+  },
+  {
+    "revision": "7f451cba289cab61e0e208378c297083",
+    "url": "/techprimer.github.io/static/media/jarvis.7f451cba.PNG"
+  },
+  {
+    "revision": "bdca5facc5493f5d0ad62697d7c4a4eb",
+    "url": "/techprimer.github.io/static/media/lms.bdca5fac.png"
+  },
+  {
+    "revision": "a08696979cad9d278d2b460bff1d0984",
+    "url": "/techprimer.github.io/static/media/login.a0869697.png"
+  },
+  {
+    "revision": "6792a061f1fcad943d35f0eda48ff46c",
+    "url": "/techprimer.github.io/static/media/self.6792a061.PNG"
+  }
+]);
